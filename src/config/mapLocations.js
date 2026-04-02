123c408429fe815e573db4b77df594bf9e6ae154
Djebcrist <djebcrist@gmail.com>
2026-04-02 21:45:23 +0200

    Feat : map location v1

--- src/config/mapLocations.js
+++ src/config/mapLocations.js
@@ -1,8 +1,15 @@
 module.exports = {
   LOCATIONS: [
-    'Todorme',
-    'Eldoria',
-    'Varnholt',
-    'Neraxis'
-  ]
+    "Shevar' District 0",
+    "Shevar' District 1",
+    "Shevar' District 2",
+    "Shevar' District 3",
+    "Shevar' District 4",
+    "Shevar' District 5",
+    "Shevar' District 6",
+    "Shevar' District 7",
+    "Shevar' District 8",
+    "Extérieur de Shevar'",
+    'Endroit Inconnu',
+  ],
 };
